aeab8281318c0dd531f167080fdcfa70b9a42ed7
Maxim Degi <unliex@yandex.ru>
2023-12-31 11:33:53 +0300

    Switched to Redis

--- states/states.py
+++ states/states.py
@@ -1,7 +1,8 @@
 from aiogram.types import CallbackQuery
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage, Redis
 from aiogram.fsm.state import StatesGroup, State
-storage = MemoryStorage()
+redis = Redis(host='localhost')
+storage = RedisStorage(redis=redis)
 
 class Schedule:
     week_schedule={'Понедельник': ['Здесь пока пусто('],'Вторник':['Здесь пока пусто('],'Среда':['Здесь пока пусто('],'Четверг':['Здесь пока пусто('],'Пятница':['Здесь пока пусто('],'Суббота':['Здесь пока пусто('],'Воскресенье':['Здесь пока пусто(']}
