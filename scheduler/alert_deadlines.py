b46a581ed7f92966dc117578dc0fd7e938e2a984
Maxim Degi <unliex@yandex.ru>
2024-02-26 07:25:17 +0300

    forgot to track "scheduler"

---
+++ scheduler/alert_deadlines.py
@@ -0,0 +1,14 @@
+from aiogram import Bot
+import logging
+
+from environs import Env
+logger = logging.getLogger(__name__)
+
+env  = Env()
+bot_token = env('BOT_TOKEN')
+bot = Bot(token=bot_token)
+
+async def schedule_deadline_alert(arqredis,chat_id,subject_task,due):
+    logger.info(('Task notification queued.',
+              await arqredis.enqueue_job('alert_deadline',chat_id, subject_task,_defer_until=due),
+                     'Deferred until', due))
