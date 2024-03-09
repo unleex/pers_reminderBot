4a7bb14a57da70f9a665bff96ea0d01a29de472d
Maxim Degi <unliex@yandex.ru>
2024-03-09 23:54:26 +0300

    Changed /schedule command to /menu

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -3,7 +3,7 @@ from aiogram import Bot
 async def set_main_menu(bot: Bot) -> None:
     main_menu_commands=[
         BotCommand(command='help', description='Информация о боте'),
-        BotCommand(command='schedule',description='Вызвать расписание'),
+        BotCommand(command='menu',description='Домашка/расписание'),
         BotCommand(command='start',description='Нажмите, eсли бот не отвечает')
     ]
     await bot.set_my_commands(main_menu_commands)
